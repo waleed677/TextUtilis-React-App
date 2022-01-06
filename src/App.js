70247f161f8b22318759c9c1fe09190dd40aa158
Waleed-Arshad-com <waleed@computan.net>
2022-01-06 21:55:34 +0500

    setup textutilis react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar title="TextUtilis" aboutText="About us" />
+    </>
+  );
+}
+
+export default App;
