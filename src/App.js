fc54de1b8730fbb185090a60dfbab3e2d10c4e9b
Waleed-Arshad-com <waleed@computan.net>
2022-01-10 18:50:17 +0500

    enable dark mode

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
+import About from "./components/About";
 
 function App() {
   return (
     <>
       <Navbar title="TextUtilis" aboutText="About us" />
-      <div className="container my-3">
-        <TextForm heading="Enter text to analyze" />
+      <div className="container my-5">
+        {/* <TextForm heading="Enter text to analyze" /> */}
+        <About />
       </div>
     </>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 export default function Navbar(props) {
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
           <a className="navbar-brand" href="/">
             {props.title}
@@ -28,17 +28,6 @@ export default function Navbar(props) {
                 </a>
               </li>
             </ul>
-            <form className="d-flex">
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button className="btn btn-outline-success" type="submit">
-                Search
-              </button>
-            </form>
           </div>
         </div>
       </nav>
