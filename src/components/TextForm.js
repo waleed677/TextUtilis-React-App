da9659a149899636c6791daa963e71913927040c
Waleed-Arshad-com <waleed@computan.net>
2022-01-10 16:46:28 +0500

    add sentence count

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -11,6 +11,22 @@ export default function TextForm(props) {
     setText(text.toUpperCase());
   };
 
+  const handleLoClick = () => {
+    setText(text.toLowerCase());
+  };
+
+  const reset = () => {
+    setText("");
+  };
+
+  const handleFirstChar = () => {
+    let words = text.split(" ").map((word) => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    });
+    let newText = words.join(" ");
+    setText(newText);
+  };
+
   return (
     <>
       <div className="mb-3">
@@ -27,12 +43,42 @@ export default function TextForm(props) {
         <button className="btn btn-primary mt-3" onClick={handleUpClick}>
           UpperCase
         </button>
+        <button className="btn btn-primary mt-3 mx-1" onClick={handleLoClick}>
+          LowerCase
+        </button>
+        <button
+          className="btn btn-primary mt-3 mx-1 "
+          onClick={handleFirstChar}
+        >
+          Capitalize First Letter
+        </button>
+        <button className="btn btn-primary mt-3 " onClick={reset}>
+          Reset
+        </button>
       </div>
 
       <div className="container">
-        <h2>
-          {text.split(" ").length - 1} words and {text.length} characters
-        </h2>
+        <h6>
+          <div className="card" style={{ width: "18rem" }}>
+            <div className="card-header">Details</div>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">
+                Words{" "}
+                <span className="rightSide"> {text.split(" ").length - 1}</span>
+              </li>
+              <li className="list-group-item">
+                Characters <span className="rightSide">{text.length} </span>
+              </li>
+              <li className="list-group-item">
+                Sentences{" "}
+                <span className="rightSide">
+                  {" "}
+                  {text.split(/[\\.!\?]/).length - 1}
+                </span>
+              </li>
+            </ul>
+          </div>
+        </h6>
       </div>
     </>
   );
